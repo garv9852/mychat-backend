849e7044feb78dd1baba11a4b9be8cadfc49cefe
garv9852 <garv.kapoor18@gmail.com>
2023-01-15 23:43:48 +0530

    v3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const server=require('http').createServer(app);
 const io=require('socket.io')(server,{
     cors:{
-        origin:"https://mychat-swart.vercel.app/",
+        origin:"*",
         method:["GET","POST"]   
     }
 });
