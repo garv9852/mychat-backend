52af16f98abb81d0c0e87ae4b56af1aadcdf5058
garv9852 <garv.kapoor18@gmail.com>
2023-01-15 19:31:05 +0530

    v2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const server=require('http').createServer(app);
 const io=require('socket.io')(server,{
     cors:{
-        origin:"*",
+        origin:"http://localhost:3000",
         method:["GET","POST"]   
     }
 });
